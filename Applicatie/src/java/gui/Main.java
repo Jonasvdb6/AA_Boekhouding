455ee52c4c9d63cb5145be71c61edc22b926547a
SimonVleugels <simon@SIMONLAPTOP-PC>
2017-01-03 22:09:54 +0100

    Gui layout af

---
+++ Applicatie/src/java/gui/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+//import SessionBean.localStatelessLocal;
+//import javax.ejb.EJB;
+
+/**
+ *
+ * @author simon
+ */
+public class Main {
+
+    //@EJB private localStatelessLocal stateless;
+    
+    public static void main(String[] args) {
+        new GUI();
+    }
+    
+}
